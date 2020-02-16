0c75e4c17aa75c7dc29b9540c1235446480405ca
Sherriff Agboola <rylxes@gmail.com>
2020-02-16 20:05:55 +0100

    fix

--- resources/views/lgas/datatables_actions.blade.php
+++ resources/views/lgas/datatables_actions.blade.php
@@ -1,9 +1,10 @@
 {!! Form::open(['route' => ['lgas.destroy', $id], 'method' => 'delete']) !!}
 <div class='btn-group'>
-
+    <a href="{{ route('lgas.show', $id) }}" class='mb-2 mr-2 btn-icon btn btn-primary'>
+        <i class="lnr-crop btn-icon-wrapper"></i>View
+    </a>
     <a href="{{ route('lgas.edit', $id) }}" class='mb-2 mr-2 btn-icon btn btn-secondary'>
         <i class="lnr-location btn-icon-wrapper"> </i> Edit
     </a>
-
 </div>
 {!! Form::close() !!}
