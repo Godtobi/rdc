87328ec39e7610a8cf6b4b839418c154cfcf0e2d
Sherriff Agboola <rylxes@gmail.com>
2020-03-22 18:24:20 +0100

    fix

--- app/DataTables/AgencyAgentDataTable.php
+++ app/DataTables/AgencyAgentDataTable.php
@@ -85,11 +85,8 @@ class AgencyAgentDataTable extends DataTable
                 'stateSave' => true,
                 'order' => [[0, 'desc']],
                 'buttons' => [
-                    ['extend' => 'create', 'className' => 'btn btn-default btn-sm no-corner',],
                     ['extend' => 'export', 'className' => 'btn btn-default btn-sm no-corner',],
                     ['extend' => 'print', 'className' => 'btn btn-default btn-sm no-corner',],
-                    ['extend' => 'reset', 'className' => 'btn btn-default btn-sm no-corner',],
-                    ['extend' => 'reload', 'className' => 'btn btn-default btn-sm no-corner',],
                 ],
             ]);
     }
--- app/DataTables/EnforcerDataTable.php
+++ app/DataTables/EnforcerDataTable.php
@@ -55,11 +55,8 @@ class EnforcerDataTable extends DataTable
                 'stateSave' => true,
                 'order' => [[0, 'desc']],
                 'buttons' => [
-                    ['extend' => 'create', 'className' => 'btn btn-default btn-sm no-corner',],
                     ['extend' => 'export', 'className' => 'btn btn-default btn-sm no-corner',],
                     ['extend' => 'print', 'className' => 'btn btn-default btn-sm no-corner',],
-                    ['extend' => 'reset', 'className' => 'btn btn-default btn-sm no-corner',],
-                    ['extend' => 'reload', 'className' => 'btn btn-default btn-sm no-corner',],
                 ],
             ]);
     }
