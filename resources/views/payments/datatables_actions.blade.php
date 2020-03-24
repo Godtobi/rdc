132f59b4fdd34620e07b1ef141165bc1f73e9d10
Sherriff Agboola <rylxes@gmail.com>
2020-03-24 17:00:47 +0100

    fix

--- resources/views/payments/datatables_actions.blade.php
+++ resources/views/payments/datatables_actions.blade.php
@@ -1,6 +1,8 @@
+@role('admin')
 <div class='btn-group'>
     <a href="{{ route('payments.show', $item->user_id) }}" class='mb-2 mr-2 btn-icon btn btn-primary'>
         <i class="lnr-crop btn-icon-wrapper"></i>View
     </a>
 
 </div>
+@endrole
